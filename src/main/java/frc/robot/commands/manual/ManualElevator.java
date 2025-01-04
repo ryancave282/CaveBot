3fd4ad51a6e07b3bd732c4be1f19ec84c10eea9d
ryancave282 <ryancave282@gmail.com>
2025-01-04 17:01:50 -0600

    Fix merge conflicts

--- src/main/java/frc/robot/commands/manual/ManualElevator.java
+++ src/main/java/frc/robot/commands/manual/ManualElevator.java
@@ -1,8 +1,7 @@
-// package frc.robot.commands.manual;
+package frc.robot.commands.manual;
 
-// import java.util.function.Supplier;
+import java.util.function.Supplier;
 
-<<<<<<< HEAD
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.DroidRageConstants;
 import frc.robot.subsystems.Elevator;
@@ -18,27 +17,10 @@ public class ManualElevator extends Command {
         
         addRequirements(elevator);
     }
-=======
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.DroidRageConstants;
-// import frc.robot.subsystems.ampMech.AmpMech;
-
-// public class ManualElevator extends Command {
-//     private final AmpMech ampMech;
-//     private final Supplier<Double> moveElev;
-    
-//     public ManualElevator(Supplier<Double> moveElev, AmpMech ampMech) {
-//         this.ampMech = ampMech;
-//         this.moveElev = moveElev;
-        
-//         addRequirements(ampMech.getElevator());
-//     }
->>>>>>> b0bfdc38be23888372d9d64e5878eb709e4fcdd7
 
 //     @Override
 //     public void initialize() { }
 
-<<<<<<< HEAD
     @Override
     public void execute() {
         // if(!isClimbing){
@@ -58,22 +40,12 @@ public class ManualElevator extends Command {
 
         // elevator.setMovingManually(!(move == 0));
     }
-=======
-//     @Override
-//     public void execute() {
-//         double move = -moveElev.get();
-//         move = DroidRageConstants.squareInput(move);
-//         move = DroidRageConstants.applyDeadBand(move);
-//         ampMech.getElevator().setTargetPosition(ampMech.getElevator().getTargetPosition() + move * 0.2);
-//         ampMech.getElevator().setMovingManually(!(move == 0));
-//     }
->>>>>>> b0bfdc38be23888372d9d64e5878eb709e4fcdd7
 
-//     @Override
-//     public void end(boolean interrupted) {}
+    @Override
+    public void end(boolean interrupted) {}
 
-//     @Override
-//     public boolean isFinished() {
-//         return false;
-//     }
-// }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}

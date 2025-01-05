84a29de92634f2687f2ec344abb75f50d23ef63f
ryancave282 <ryancave282@gmail.com>
2025-01-04 21:00:15 -0600

    Create Coral

---
+++ src/main/java/frc/robot/subsystems/coral/Coral.java
@@ -0,0 +1,79 @@
+package frc.robot.subsystems.coral;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class Coral {
+    public enum Value{
+        START(0, 0, 0);
+
+        private final double armAngle;
+        private final double pivotAngle;
+        private final double intakeSpeed;
+
+        private Value(double armAngle, double pivotAngle, double intakeSpeed){
+            this.armAngle = armAngle;
+            this.pivotAngle = pivotAngle;
+            this.intakeSpeed = intakeSpeed;
+        }
+        
+        public double getArmAngle(){
+            return armAngle;
+        }
+
+        public double getPivotAngle(){
+            return pivotAngle;
+        }
+
+        public double getIntakeSpeed(){
+            return intakeSpeed;
+        }
+    }
+
+    public CoralArm arm;
+    public CoralPivot pivot;
+    public CoralIntake intake;
+
+    @SuppressWarnings("unused")
+    private Value position = Value.START;
+
+    public Coral(CoralArm arm, CoralPivot pivot, CoralIntake intake){
+        this.arm = arm;
+        this.pivot = pivot;
+        this.intake = intake;
+    }
+
+    public Command setPositionCommand(Value targetPos) {
+        position = targetPos;
+        return Commands.sequence(
+            switch (targetPos) {
+                case START -> 
+                    new SequentialCommandGroup(
+                        arm.setTargetPositionCommand(targetPos.getArmAngle()),
+                        pivot.setTargetPositionCommand(targetPos.getPivotAngle()),
+                        intake.setTargetPositionCommand(targetPos.getIntakeSpeed())
+                    );
+                default -> 
+                    new ParallelCommandGroup(
+                        arm.setTargetPositionCommand(targetPos.getArmAngle()),
+                        pivot.setTargetPositionCommand(targetPos.getPivotAngle()),
+                        intake.setTargetPositionCommand(targetPos.getIntakeSpeed())
+                    );
+            }
+        );
+    }
+
+    public CoralArm getCoralArm(){
+        return arm;
+    }
+
+    public CoralPivot getCoralPivot(){
+        return pivot;
+    }
+
+    public CoralIntake getCoralIntake(){
+        return intake;
+    }
+}
--- src/main/java/frc/robot/subsystems/coral/CoralPivot.java
+++ src/main/java/frc/robot/subsystems/coral/CoralPivot.java
@@ -11,25 +11,32 @@ import frc.robot.utility.motor.TalonEx;
 import frc.robot.utility.template.ArmAbsoluteTemplate;
 
 public class CoralPivot extends ArmAbsoluteTemplate {
+    public static class Constants {
+        public static final double MAX_POSITION = 0;
+        public static final double MIN_POSITION = 0;
+        public static final double OFFSET = 0;
+    }
+    
     private static TalonEx motor = TalonEx.create(0)
         .withDirection(Direction.Forward)
         .withIdleMode(ZeroPowerMode.Coast)
         .withPositionConversionFactor(1)
-        .withSubsystemName("Coral Pivot")
+        .withSubsystemName("Coral")
         .withIsEnabled(true)
         .withSupplyCurrentLimit(50);
     
     private static AbsoluteDutyEncoderRIO encoder = AbsoluteDutyEncoderRIO.create(0)
         .withDirection(false)
         .withOffset(0)
-        .withSubsystemBase("Coral Pivot");
+        .withSubsystemBase("Coral");
 
     public CoralPivot() {
         super(
         new CANMotorEx[]{motor}, 
         new PIDController(0,0,0), 
-        new ArmFeedforward(0, 0, 0, 0, 0), 0, 0, 0, 
-        Control.PID, "Coral Pivot", 0, encoder);
+        new ArmFeedforward(0, 0, 0, 0, 0), 
+        Constants.MAX_POSITION, Constants.MIN_POSITION, Constants.OFFSET, 
+        Control.PID, "Coral", 0, encoder);
         
     }
 }
